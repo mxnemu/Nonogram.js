f2c82db13a2e7dfb6aad1e3c82af74ef830f7b58
Georg Meyer <meyer@meyer.(none)>
2011-12-09 18:04:22 +0100

    Added src directory and files; Added wrapper methods for Application and Board;

---
+++ src/Application.js
@@ -0,0 +1,30 @@
+/**
+ * Application constructor
+ */
+function Application() {}
+
+
+
+
+/**
+ * Saves a game under a name.
+ *
+ * @param String sName game name
+ * @param Object oGame game object
+ */
+Application.prototype.save = function (sName, oGame) {
+    
+};
+
+
+
+
+/**
+ * Returns a game by name.
+ *
+ * @param String sName game name
+ * @return Object game object
+ */
+Application.prototype.load = function (sName) {
+
+};
---
+++ src/Board.js
@@ -0,0 +1,43 @@
+/**
+ * Board constructor
+ * 
+ * @param Integer iWidth board width
+ * @param Integer iHeight board height
+ */
+function Board(iWidth, iHeight) {
+    this.iWidth = iWidth;
+    this.iHeight = iHeight;
+}
+
+
+
+
+/**
+ * 
+ */
+Board.prototype.createNodes = function () {
+    
+};
+
+
+
+
+/**
+ * 
+ */
+Board.prototype.updateNodes = function () {
+    
+};
+
+
+
+
+/**
+ * 
+ */
+Board.prototype.removeNodes = function () {
+    
+};
+
+
+
