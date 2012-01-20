433323482e1e7959f4e63f5a2f78f3f8e5397a56
Timm Brinkmann <timm@mousemail.de>
2012-01-20 06:27:39 +0100

    GameBoard properties korrigiert

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -5,22 +5,18 @@
  * @param Object oHistory game history
  * @param Object oSolution game solution snapshot
  */
-function GameBoard(iWidth, iHeight, oDifficulty, oHistory, oSolution) {
+function GameBoard(iWidth, iHeight) {
     Board.call(this, iWidth, iHeight, CellStatus.UNMARKED); // parent constructor
-
-    this.oDifficulty = oDifficulty;
-    this.oHistory = oHistory;
-    this.oSolution = oSolution;
 }
 
 GameBoard.prototype = new Board(); // inherit
 
 GameBoard.prototype.onClick = function(x, y) {
     this.setCellStatus(x, y, this.getCellStatus(x, y) !== CellStatus.ACTIVE ? CellStatus.ACTIVE : CellStatus.INACTIVE, true);
-    if (!this.oHistory.getCurrent.isValid()) {
+    if (!this.history.getCurrent().isValid()) {
         // Fehlerposition speichern
     }
-    if (this.oHistory.getCurrent.isEqual(this.solution))
+    if (this.history.getCurrent().isEqual(this.solution))
         alert("Herzlichen Glückwunsch, Sie haben gewonnen!");
 };
 
