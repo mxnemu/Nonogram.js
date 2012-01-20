730400275b3e37276bed6276a61b6665f2ee2470
Johannes Wüller <johanneswueller@gmail.com>
2012-01-20 05:03:23 +0100

    styled editor interface

--- src/EditorScreen.js
+++ src/EditorScreen.js
@@ -19,21 +19,30 @@ EditorScreen.prototype.createOptionNode = function() {
     this.optionNode = $(
           '<div class="option-node" id="option-node-editor">'
             + '<h2>EDITOR</h2>'
-            + '<input name="editor-gamename" type="text" />'
-            + '<input name="editor-load" type="button" value="load" />'
-            + '<input name="editor-save" type="button" value="save" />'
+            + '<fieldset>'
+                + '<legend>Create</legend>'
+                + '<table>'
+                    + '<tr><th>Width</th><td><input name="editor-width" type="text" /></td></tr>'
+                    + '<tr><th>Height</th><td><input name="editor-height" type="text" /></td></tr>'
+                + '</table>'
+                + '<input name="editor-create" type="button" value="create" />'
+            + '</fieldset>'
+            + '<fieldset>'
+                + '<legend>Save</legend>'
+                + '<table>'
+                    + '<tr><th>Name</th><td><input name="editor-gamename" type="text" /></td></tr>'
+                + '</table>'
+                + '<input name="editor-save" type="button" value="save" />'
+            + '</fieldset>'
+            + '<fieldset>'
+                + '<legend>Load</legend>'
+                + 'list goes here'
+            + '</fieldset>'
         + '</div>'
     );
 
-    this.optionNode.find('input[name="editor-load"]').click(function() {
-        _this.load(_this.optionNode.find('input[name="editor-gamename"]').val());
-    });
-
     this.optionNode.find('input[name="editor-save"]').click(function() {
-        _this.save(
-            _this.optionNode.find('input[name="editor-gamename"]').val()
-            , _this
-        );
+        _this.save(_this.optionNode.find('input[name="editor-gamename"]').val(), _this);
     });
 
     return this.optionNode;
