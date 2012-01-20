89fbce39a0f74455d6797385976307de871c29c5
Johannes Wüller <johanneswueller@gmail.com>
2012-01-20 04:30:25 +0100

    fixed GameBoard

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -6,7 +6,6 @@
  * @param Object oSolution game solution snapshot
  */
 function GameBoard(iWidth, iHeight, oDifficulty, oHistory, oSolution) {
-    // TODO: Insert real width and height.
     Board.call(this, iWidth, iHeight); // parent constructor
 
     this.oDifficulty = oDifficulty;
@@ -19,13 +18,13 @@ GameBoard.prototype = new Board(); // inherit
 /**
  * onClick
  */
-EditorBoard.prototype.onClick = function(x, y) {
+GameBoard.prototype.onClick = function(x, y) {
     this.setCellStatus(x, y, this.getCellStatus(x, y) == CellStatus.ACTIVE? CellStatus.INACTIVE : CellStatus.ACTIVE);
 };
 
 /**
  * onRightClick
  */
-EditorBoard.prototype.onRightClick = function(x, y) {
+GameBoard.prototype.onRightClick = function(x, y) {
     this.setCellStatus(x, y, CellStatus.TAGGED);
 };
