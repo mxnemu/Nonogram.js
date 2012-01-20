0e3c663577a2fae4e761397700f65dcc62a72018
Johannes Wüller <johanneswueller@gmail.com>
2012-01-20 03:33:38 +0100

    added Screen.redraw()

--- src/Screen.js
+++ src/Screen.js
@@ -1,10 +1,14 @@
 function Screen(board) {
     this.board = board || null;
-    
-    if (this.board !== null) {
+    this.redraw();
+}
+
+Screen.prototype.redraw = function () {
+    if (this.board === null) {
+        this.board.removeNodes();
         $('#playground').append(this.board.createNodes());
     }
-}
+};
 
 Screen.prototype.save = function (name) {
     if (this.board === null) throw "Could not save `" + name + "'.";
