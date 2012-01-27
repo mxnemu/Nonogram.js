44758bdb7e45555f19f7faecab2fb2b3e582930a
Nehmulos <Nehmulos@inbox.com>
2012-01-27 12:46:32 +0100

    documented all the functions;

--- src/EditorBoard.js
+++ src/EditorBoard.js
@@ -7,6 +7,10 @@ function EditorBoard(iWidth, iHeight) {
 
 EditorBoard.prototype = new Board(); // inherit
 
+/**
+ * Update the SolutionSnapshot when setting the cellStatus on the board.
+ * This also updates the hints displayed on the borders of the board.
+ */
 EditorBoard.prototype.setCellStatus = function () {
     Board.prototype.setCellStatus.apply(this, Array.prototype.slice.call(arguments));
 
@@ -14,11 +18,21 @@ EditorBoard.prototype.setCellStatus = function () {
     this.setSolution(this.history.getCurrent());
 };
 
+/**
+ * Handles the behaviour when a mouse with the leftMouseButton down enters a cell.
+ * Set the Cellstatus to ACTIVE if a SolutionSnapshot exists that can be modified,
+ * otherwise do nothing.
+ */
 EditorBoard.prototype.onEnter = function(x, y) {
     if (!this.solution) return;
     this.setCellStatus(x, y, CellStatus.ACTIVE, true);
 };
 
+/**
+ * Handles the behaviour when a mouse with the rightMouseButton down enters a cell.
+ * Set the Cellstatus to INACTIVE if a SolutionSnapshot exists that can be modified,
+ * otherwise do nothing.
+ */
 EditorBoard.prototype.onRightEnter = function(x, y) {
     if (!this.solution) return;
     this.setCellStatus(x, y, CellStatus.INACTIVE, true);
